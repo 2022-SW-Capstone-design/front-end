04708a42bc0e4ba74d4d1c6d718146f52b8c7840
sangjun-park-201520131 <101028399+sangjun-park-201520131@users.noreply.github.com>
2022-05-23 16:39:06 +0900

    replace URL

--- src/components/http-request.js
+++ src/components/http-request.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const baseURL =
-  "http://ec2-52-78-142-179.ap-northeast-2.compute.amazonaws.com:8081/";
+  "http://ec2-52-78-142-179.ap-northeast-2.compute.amazonaws.com:8081";
 
 export const getData = async (url, bearerToken) => {
   const response = await axios.get(`${baseURL}/${url}`, {
